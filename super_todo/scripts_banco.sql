c1dac887d53d65008fdae6998743950af7a0293e
Thiago G. Traue <t.traue@gmail.com>
2021-04-22 11:18:58 -0300

    Update scripts_banco.sql

--- super_todo/scripts_banco.sql
+++ super_todo/scripts_banco.sql
@@ -5,3 +5,11 @@ CREATE TABLE tb_status(
 
 INSERT INTO tb_status(status) VALUES ('pendente');
 INSERT INTO tb_status(status) VALUES ('realizado');
+
+CREATE TABLE tb_tarefas(
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    tarefa TEXT NOT NULL,
+    data_cadastro DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    id_status INT NOT NULL DEFAULT 1,
+    FOREIGN KEY(id_status) REFERENCES tb_status(id)
+);
